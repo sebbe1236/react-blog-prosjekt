c9cfe7bc7a0e6c14ea5ef0db5f92de0219d7efd5
sebbe1236 <74545889+sebbe1236@users.noreply.github.com>
2022-07-17 16:03:06 +0200

    loaders and error messsages

--- src/components/pages/Blogs/Blogs.jsx
+++ src/components/pages/Blogs/Blogs.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import BlogsContent from "./BlogsContent";
 import { BASE_URL } from "../../constants/api";
+// query url kode : url + `?q=${query}`
+//Prøv med strapi api iløpet av uka evenntuelt
+//teste denn hher evt: https://www.freecodecamp.org/news/search-and-filter-component-in-reactjs/
+//kildde brukt nå:https://www.youtube.com/watch?v=MY6ZZIn93V8&t=1214s&ab_channel=LamaDev
 
 function Blogs() {
   const [query, setQuery] = useState("");
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const url = BASE_URL;
-  // query url kode : url + `?q=${query}`
+
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
@@ -17,10 +23,20 @@ function Blogs() {
         setBlogs(results);
       } catch (err) {
         console.log(err, "Funka ikkee");
+        setError(err.toString());
+      } finally {
+        setLoading(false);
       }
     };
     fetchBlogs();
   }, [query]);
+
+  if (loading) {
+    return <h1>Loading.....</h1>;
+  }
+  if (error) {
+    return <p>Something went to shhit</p>;
+  }
 
   return (
     <>
